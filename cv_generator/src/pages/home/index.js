bb6b8dbb2d295daeda939115091363144bdf988b
georgian13 <hrgeorgian13@gmail.com>
2019-01-28 20:46:32 +0400

    2
    Create background image

--- cv_generator/src/pages/home/index.js
+++ cv_generator/src/pages/home/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { URL } from '../../config/constants';
+import { Button, Link } from 'react-bootstrap';
 import './index.css';
 
 export default class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.changeRouter = this.changeRouter.bind(this);
+	}
+	changeRouter(url) {
+		this.props.history.push(url);
+	}
+
 	render() {
 		return (
 			<div className = 'home'>
-				<button className = 'createButton'>Create New Resume
-				</button>
+				<Button 
+					bsStyle="success" 
+					onClick ={ () => this.changeRouter(URL.PERSONAL_INFO) }>
+					Create New Resume
+				</Button>
 			</div>
 		);
 	}
